767505bdb5fa4c765edc7da6178bff3e8f0c1557
Sujitha S <sujithagayathiri007@gmail.com>
2025-12-24 18:42:44 +0530

    preprocess.py

--- unet_segmentation/preprocess.py
+++ unet_segmentation/preprocess.py
@@ -1,1 +1,22 @@
+import cv2
+import os
+import numpy as np
+
+IMG_DIR = "segmentation_dataset/images"
+MASK_DIR = "segmentation_dataset/masks"
+IMG_SIZE = 256
+
+def preprocess():
+    for folder in [IMG_DIR, MASK_DIR]:
+        for file in os.listdir(folder):
+            if file.endswith(".png"):
+                path = os.path.join(folder, file)
+                img = cv2.imread(path, cv2.IMREAD_GRAYSCALE if folder == MASK_DIR else cv2.IMREAD_COLOR)
+                img = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
+                cv2.imwrite(path, img)
+
+    print(" Preprocessing completed (256x256 resize)")
+
+if __name__ == "__main__":
+    preprocess()
 
